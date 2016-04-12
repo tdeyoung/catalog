00be7eae112241e0f4af11d922abb46e98e119bf
Tim DeYoung <tim.deyoung@yahoo.com>
2016-04-12 00:19:31 -0400

    Validation on the Item table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
+	validates :title, presence: true, length: {minimum: 3, maximum: 80}
+	validates :composer_lname, presence: true
+	validates :location, presence: true
+
 	enum ensemble: [:band, :choir, :general_music, :orchestra, :other]
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@item = Item.create(title: "a title goes here", composer_fname: "fname", composer_lname: "lname", 
+			ensemble: 1, location: "location")
+	end
+
+	test "item must be vaild" do
+		assert @item.valid?	
+	end
+
+	test "title must be present" do
+		@item.title = ""
+		assert_not @item.valid?
+	end
+
+	test "title must not be too short" do
+		@item.title = "aa"
+		assert_not @item.valid?
+	end
+
+	test "title must not be too long" do
+		@item.title = "a" * 81
+		assert_not @item.valid?
+	end
+
+	test "composer last name must be present" do
+		@item.composer_lname = "" 
+		assert_not @item.valid?
+	end
+
+	test "ensemble must be valid" do
+		invalid_ensembles = [-10, -1, 5, 10]
+		invalid_ensembles.each do |is|
+			begin
+				@item.ensemble = is
+				assert false, "#{is} should be invalid"
+			rescue
+				assert true
+			end
+		end
+	end
+
+	test "location must be present" do
+		@item.location = ""
+		assert_not @item.valid?
+	end
+
 end

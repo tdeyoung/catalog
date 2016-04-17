112f4506b3b7b556a8ebd1b9663069efde51957f
Tim DeYoung <tim.deyoung@yahoo.com>
2016-04-16 23:16:47 -0400

    update code to 4.4.2 in rails tutorial

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | GRPS Music Catalog"
+    assert_select "title", "GRPS Music Catalog"
   end
 
   test "should get help" do

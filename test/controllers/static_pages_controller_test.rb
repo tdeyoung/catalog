cc0b4b0a621f2ba042087a67ccf1b58314e9ef6d
Tim DeYoung <tim.deyoung@yahoo.com>
2016-06-28 14:19:43 -0400

    fix test failures

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "GRPS Music Catalog"
+    assert_select "title", "ScoreShare"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | GRPS Music Catalog"
+    assert_select "title", "Help | ScoreShare"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | GRPS Music Catalog"
+    assert_select "title", "About | ScoreShare"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | GRPS Music Catalog"
+    assert_select "title", "Contact | ScoreShare"
   end
 end

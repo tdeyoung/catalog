65ca9b2788b12757e5ae856c93174ea9a3e81c25
Tim DeYoung <tim.deyoung@yahoo.com>
2016-07-21 11:37:51 -0400

    user profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
  # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 4)
   end
   
   def new

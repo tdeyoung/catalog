b06bea42c709f612b679540966784c03b9808351
Tim DeYoung <tim.deyoung@yahoo.com>
2016-07-23 11:43:54 -0400

    on-boarding two

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,6 @@ class UsersController < ApplicationController
   end
   
   def create
-
-    #binding pry
     @user = User.new(user_params)
   	if @user.save
   		flash[:success] = "Your account has been created succesfully"

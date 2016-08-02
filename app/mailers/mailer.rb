5c933544e323faab111c9b6d10295748a5ea230a
Tim DeYoung <tim.deyoung@yahoo.com>
2016-08-02 13:24:51 -0400

    correct typos in action mailer code

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,4 +1,4 @@
-class Mailer < ApplicationMailer
+class Mailer < ApplicationMailer::Base
 	default from: "musiccatalogdev@gmail.com"
 
 	def sample_email(user)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  #SMPT settings for gmail
-  config.action_mailer.smpt_settings = {
+  #SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
     :address          => "smtp.gmail.com",
     :port             => 587,
     :user_name        => ENV['gmail_username'],
